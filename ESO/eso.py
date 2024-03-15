8874a2ae7ece3ba3abd3f934085671fdc2d7e55a
Venelin Atanasov <venelin.atanasov2@gmail.com>
2024-03-15 02:55:17 +0200

    eso structure

---
+++ ESO/eso.py
@@ -0,0 +1,20 @@
+class eso:
+    def __init__(self, prices, availability, consumption, subscribers):
+        self.prices=prices
+        self.availability=availability
+        self.consumption=consumption
+        self.subscribers=subscribers
+        
+
+    def send_prices(prices):
+        pass
+
+    def send_grid_status(consumption):
+        pass
+
+    def cut_subscriber(subscribers):
+        pass
+
+    def retrieve_consumption():
+        pass
+    
